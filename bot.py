a89129bfbf0d1dc40be52fa9d39ae7232e920958
Álvaro Garrido <alvarospunk@hotmail.com>
2019-01-27 18:25:08 +0100

    all: insert teams

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import pymysql
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import secrets
 import random
+import pdb
 
 # Create a connection object
 dbServerName    = "127.0.0.1"
@@ -15,6 +16,32 @@ cursorType      = pymysql.cursors.DictCursor
 
 connectionObject   = pymysql.connect(host=dbServerName, user=dbUser, password=dbPassword,
                                         db=dbName, charset=charSet,cursorclass=cursorType)
+
+original_teams = []
+
+def fetch_teams():
+    try:
+        # Create a cursor object
+        cursorObject = connectionObject.cursor()
+
+        # SQL query string
+        sqlQuery = "SELECT * FROM teams;"
+
+        # Execute the sqlQuery
+        cursorObject.execute(sqlQuery)
+
+        #Fetch all the rows
+        rows = cursorObject.fetchall()
+        for row in rows:
+            try:
+                original_teams.append(row['team_id'])
+            except Exception:
+                print("Exception occurred fetching field 'team_id'")
+
+    except Exception:
+        print("Exception occurred in SQL Query fetching teams")
+
+    #connectionObject.close()
 
 def hello(bot, update):
     update.message.reply_text(
@@ -42,78 +69,77 @@ def show_teams(bot, update):
 
     except Exception:
         bot.send_message(chat_id=update.message.chat_id, text="Exception occurred in SQL Query showing teams")
-    finally:
-        connectionObject.close()
+    # finally:
+    #     connectionObject.close()
 
 def create_tournament(bot, update):
-    table_uuid = secrets.token_hex(16)
-    original_teams = []
     chosen_teams = []
     num_teams = 16
     bot.send_message(chat_id=update.message.chat_id, text="You are about to create a new tournament!!")
     bot.send_message(chat_id=update.message.chat_id, text="Selecting 16 random teams...")
 
+    fetch_teams()
+
+    while len(chosen_teams) < num_teams:
+        try:
+            random_choice = random.choice(original_teams)
+            # Append if it's not already in the list
+            if not chosen_teams.__contains__(random_choice):
+                chosen_teams.append(random_choice)
+        except Exception:
+                print("Exception occurred choosing teams")
+
     try:
         # Create a cursor object
-        cursorObject = connectionObject.cursor()
+        cursorObject2 = connectionObject.cursor()
 
         # SQL query string
-        sqlQuery = "SELECT * FROM teams"
+        table_uuid = secrets.token_hex(4)
+        sqlQuery = "CREATE TABLE " + table_uuid + " (matchid varchar(25) not null, localteamid varchar(25) not null, visitorteamid varchar(25) not null, localuser varchar(25) not null, visitoruser varchar(25) not null, goalslocal int, goalsvisitor int, winner varchar(25));"
+        print(sqlQuery)
 
         # Execute the sqlQuery
-        cursorObject.execute(sqlQuery)
+        cursorObject2.execute(sqlQuery)
 
         #Fetch all the rows
-        rows = cursorObject.fetchall()
+        rows = cursorObject2.fetchall()
         for row in rows:
-            try:
-                original_teams.append(row['team_id'])
-            except Exception:
-                bot.send_message(chat_id=update.message.chat_id, text="Exception occurred fetching field 'team_id'")
+            print (row['team_id'])
 
-        for i in range(num_teams):
-            try:
-                random_choice = random.choice(original_teams)
-                # Append if it's not already in the list
-                if chosen_teams.__contains__(random_choice):
-                    chosen_teams.append(random_choice)
-            except Exception:
-                bot.send_message(chat_id=update.message.chat_id, text="Exception occurred choosing teams")
+    except Exception as e:
+        print("Exeception occured:{}".format(e))
 
-        for i in chosen_teams:
-            print (i)
+    # finally:
+    #     connectionObject.close()
 
-    except Exception:
-        bot.send_message(chat_id=update.message.chat_id, text="Exception occurred in SQL Query fetching teams")
-    finally:
-        connectionObject.close()
+    random.shuffle(chosen_teams)
 
-    bot.send_message(chat_id=update.message.chat_id, text="Creating tournament with UUID: " + table_uuid)
-    
-    try:
-        # Create a cursor object
-        cursorObject = connectionObject.cursor()
+    cursorObjectList = []
 
-        # SQL query string
-        sqlQuery = """CREATE TABLE """ + table_uuid + """(
-            match_id varchar(25) unique not null,
-            local_team_id varchar(25) unique not null,
-            visitor_team_id varchar(25) unique not null,
-            goals_local int,
-            goals_visitor int,
-            winner varchar(25))"""
-        bot.send_message(chat_id=update.message.chat_id, text=sqlQuery)
+    for i in range(len(chosen_teams)):
+        cursorObjectList.append(connectionObject.cursor())
 
-        # Execute the sqlQuery
-        cursorObject.execute(sqlQuery) 
+    iters = 0
+    while len(chosen_teams) > 0:
+        try:
 
-        #Fetch all the rows
-        rows = cursorObject.fetchall()
+            team1 = chosen_teams[0]
+            chosen_teams.pop(0)
+            team2 = chosen_teams[0]
+            chosen_teams.pop(0)
+            match_uuid = secrets.token_hex(4)
 
-    except Exception:
-        bot.send_message(chat_id=update.message.chat_id, text="Exception occurred in SQL Query creating team table")
-    finally:
-        connectionObject.close()
+            # SQL query string
+            sqlQuery = "INSERT INTO " + table_uuid + " values ('" + match_uuid + "', '" + team1 + "', '" + team2 + "', 'alvaro', 'killo', 0, 0, 'None');"
+            print(sqlQuery)
+
+            # Execute the sqlQuery
+            cursorObjectList[iters].execute(sqlQuery)
+            print ("Team chosen")
+            iters=iters+1
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
 
 def update_next_match(bot, update):
     abs
@@ -124,7 +150,6 @@ def echo(bot, update):
 def answer(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="No contesto a tonterias")
 
-#updater = Updater('462567247:AAEs76XLVZZeTesKJBShttI-XQASUic8yVU')
 updater = Updater('710743623:AAEaG33UZd78fTEuZqRPIKmv8Dgn4C81-LM')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
